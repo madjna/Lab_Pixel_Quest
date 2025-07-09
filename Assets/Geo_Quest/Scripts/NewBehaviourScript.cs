292ae84457b0513a6267a14924e3318f2e8ba43c
madjna <madinaikramova2019@gmail.com>
2025-07-09 12:12:23 -0400

    7/9/25

---
+++ Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private int var0ne = 1;
+    string bear = "How are you";
+    // Start is called before the first frame update
+    void Start()
+    {
+        int var2 = 2;
+        Debug.Log("Hello World");
+        string goat = "I'm good, how about you?";
+        Debug.Log(bear+goat);
+
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
