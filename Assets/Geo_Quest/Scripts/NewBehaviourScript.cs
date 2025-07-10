080b5092c4ec96e5a69c3099525d43d60f58c654
madjna <madinaikramova2019@gmail.com>
2025-07-10 12:16:39 -0400

    7/10/25

--- Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
+++ Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
@@ -4,23 +4,46 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private int var0ne = 1;
-    string bear = "How are you";
+    private Rigidbody2D rb;
+    int varTwo = 3;
+    int speed = 3;
     // Start is called before the first frame update
     void Start()
     {
-        int var2 = 2;
-        Debug.Log("Hello World");
-        string goat = "I'm good, how about you?";
-        Debug.Log(bear+goat);
+       rb = GetComponent<Rigidbody2D>();
 
-       
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
     }
+        private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+
+    /*
+    if (Input.GetKeyDown(KeyCode.W))
+    {
+        transform.position += new Vector3(0, 1, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.S))
+    {
+        transform.position += new Vector3(0, -1, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.D))
+    {
+        transform.position += new Vector3(1, 0, 0);
+    }
+    if (Input.GetKeyDown(KeyCode.A))
+    {
+        transform.position += new Vector3(-1, 0, 0);
+    }
+    */
+
 }
